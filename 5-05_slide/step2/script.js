8384fd12bd62d642c49f5f7d22922f79e53d1e19
homerowpluto <homerowpluto@gmail.com>
2021-12-01 02:34:42 +0900

    Add 5-05_slide step2 and extra

---
+++ 5-05_slide/step2/script.js
@@ -0,0 +1,21 @@
+let images = ['../assets/images/image1.jpg', '../assets/images/image2.jpg', '../assets/images/image3.jpg', '../assets/images/image4.jpg', '../assets/images/image5.jpg']
+let current = 0;
+let pageNum = function() {
+  document.getElementById('page').textContent = (current + 1) + '/' + images.length
+}
+let changeImage = function(num) {
+  if (current + num >= 0 && current + num < images.length) {
+    current += num;
+    document.getElementById('main_image').src = images[current];
+    pageNum();
+  }
+};
+
+pageNum();
+
+document.getElementById('prev').onclick = function() {
+  changeImage(-1);
+}
+document.getElementById('next').onclick = function () {
+  changeImage(1);
+}
