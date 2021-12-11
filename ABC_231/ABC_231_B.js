c7cd428672ae8e4b755ebde4971d38afa5f24b5e
homerowpluto <homerowpluto@gmail.com>
2021-12-12 00:13:22 +0900

    remove comment

--- ABC_231/ABC_231_B.js
+++ ABC_231/ABC_231_B.js
@@ -18,34 +18,3 @@ function Main(input) {
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
-
-// Below is my attempt at B, above is a referenced more smart version
-// // parameter "input" gets all data
-// function Main(input) {
-//   let str = input.split('\n');
-//   let N = parseInt(str[0], 10);
-
-//   const names = [];
-//   const votes = [];
-//   names[0] = '';
-//   votes[0] = 0;
-//   names[1] = str[1];
-//   votes[1] = 1;
-//   for (let i = 2; i <= N; i++) {
-//     for (let j = 1; j <= names.length; j++) {
-//       if (String(names[j]).localeCompare(String(str[i])) == 0) {
-//         votes[j]++;
-//       } else if (j === names.length - 1) {
-//         names.push(str[i]);
-//         votes.push(1);
-//       }
-//     }
-//   }
-
-//   const max = Math.max(...votes);
-//   const indexOfMax = votes.indexOf(max);
-
-//   console.log('%s', names[indexOfMax]);
-// }
-// // Don't edit this line!
-// Main(require("fs").readFileSync("/dev/stdin", "utf8"));
